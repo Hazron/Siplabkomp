4a6d1096440fb1143dbca68ea4e2e138c370c0ab
Hazron <hazron2003redian@gmail.com>
2024-04-24 18:54:01 +0700

    feat: controller, view, route HOME

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('usertype');
+            $table->string('nama');
+            $table->string('password');
+            $table->string('nim');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('nomor_hp');
+            $table->string('angkatan');
+            $table->id('id_jadwal');
             $table->rememberToken();
             $table->timestamps();
         });
